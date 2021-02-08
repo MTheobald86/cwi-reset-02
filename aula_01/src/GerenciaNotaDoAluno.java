726edae4828d8c572580bb91bea8ea604116e14a
MTheobald86 <maycon.theobald@outlook.com>
2021-02-07 21:57:52 -0300

    correção do exercício 3

--- aula_01/src/GerenciaNotaDoAluno.java
+++ aula_01/src/GerenciaNotaDoAluno.java
@@ -5,7 +5,7 @@ public class GerenciaNotaDoAluno {
         Aluno aluno1 = new Aluno("José das Couves", 6.9);
 
 
-        System.out.println ("O aluno " + aluno1.getNomeDoAluno() + " obteve nota final" + aluno1.getNotaFinalDoAluno() + ". Aprovado? " + aluno1.isAprovado());
+        System.out.println ("O aluno " + aluno1.getNomeDoAluno() + " obteve nota final " + aluno1.getNotaFinalDoAluno() + ". Aprovado? " + aluno1.isAprovado());
 
     }
 }
